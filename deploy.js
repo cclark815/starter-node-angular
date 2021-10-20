84d83eb56747018a1782f50b314fc48ec8d79006
cclark815 <c.aubin.1987@gmail.com>
2021-10-20 18:56:25 -0500

    updated file

--- deploy.js
+++ deploy.js
@@ -1,9 +1,9 @@
 var cmd = require('node-cmd');
 var path, node_ssh, ssh, fs;
 fs = require('fs');
-path = require('path');
-node_ssh = require('node-ssh');
-ssh = new node_ssh.NodeSSH(); 
+path = require('path'); 
+const {NodeSSH}= require('node-ssh');
+ssh = new NodeSSH(); 
 
 // the method that starts the deployment process
 function main() {
